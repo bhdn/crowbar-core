4da0705d691069c7ec191b55c861103c2c2cc1f7
Bogdano Arendartchuk <barendartchuk@suse.com>
2019-12-03 15:56:08 +0100

    ohai: match endianness in libvirt uuid due to smbios >= 2.6
    http://dnaeon.github.io/convert-big-endian-uuid-to-middle-endian/

--- chef/cookbooks/ohai/files/default/plugins/linux/crowbar_libvirt.rb
+++ chef/cookbooks/ohai/files/default/plugins/linux/crowbar_libvirt.rb
@@ -6,6 +6,11 @@ require_plugin "linux::s390x"
 
 libvirt_uuid = nil
 
+def convert(old)
+   m = /(\h\h)(\h\h)(\h\h)(\h\h)-(\h\h)(\h\h)-(\h\h)(\h\h)-(\h{4})-(\h{12})/.match(old)
+   [m[4] + m[3] + m[2] + m[1], m[6] + m[5], m[8] + m[7], m[9], m[10]].join("-").downcase
+end
+
 if kernel[:machine] == "s390x"
   if s390x[:system][:manufacturer] == "KVM"
     libvirt_uuid = s390x[:system][:uuid]
@@ -13,7 +18,7 @@ if kernel[:machine] == "s390x"
 else
   manufacturer = dmi[:system] ? dmi[:system][:manufacturer] : "unknown"
   if ["Bochs", "QEMU"].include? manufacturer
-    libvirt_uuid = dmi[:system][:uuid]
+    libvirt_uuid = convert(dmi[:system][:uuid])
   end
 end
 
